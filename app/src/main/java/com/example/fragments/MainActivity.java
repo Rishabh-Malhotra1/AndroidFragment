bfa83d02146148c79bd30547492ae6088fa51496
RishabhMalhotra1 <rishabh.malhotra@rsllearning.com>
2021-05-19 22:28:59 +0530

    Third Commit

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.example.fragments.Frgmnts.activity1;
+import com.example.fragments.Frgmnts.activity2;
+import com.example.fragments.Frgmnts.activity3;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button1,button2,button3;
+    LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        linearLayout = findViewById(R.id.linearlayout);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity1 firstFragment = new activity1();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearlayout,firstFragment);
+                transaction.commit();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity2 firstFragment = new activity2();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearlayout,firstFragment);
+                transaction.commit();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity3 firstFragment = new activity3();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearlayout,firstFragment);
+                transaction.commit();
+            }
+        });
     }
 }
